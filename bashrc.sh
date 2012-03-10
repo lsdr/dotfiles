24b979abe17e7e9c0e113d9cb73c13b8ce4f00f6
Luiz Rocha <lsdrocha@gmail.com>
2012-03-10 10:23:17 -0300

    updating ec2-api-tools

--- bashrc.sh
+++ bashrc.sh
@@ -61,7 +61,7 @@ export PIP_RESPECT_VIRTUALENV=true
 # amazon web services
 export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
 export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
-export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.5.2.3/jars"
+export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.5.2.4/jars"
 export EC2_URL='https://ec2.sa-east-1.amazonaws.com'
 
 # git prompt pimpin'
