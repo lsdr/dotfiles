fa8dd7a199e3ab60d55a5e16fc1f0786e77d9ea1
Luiz Rocha <lsdrocha@gmail.com>
2012-08-08 17:00:12 -0300

    guess what?! updating ec2-api-tools... have to figure out a better way to do this

--- bashrc.sh
+++ bashrc.sh
@@ -72,7 +72,7 @@ export VIRTUALENV_DISTRIBUTE=true
 # amazon web services
 export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
 export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
-export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.5.5.0/jars"
+export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.6.1.2/jars"
 export EC2_URL='https://ec2.sa-east-1.amazonaws.com'
 
 # git prompt pimpin'
